e50302964973c33c5ca710b207872bfea5d760b2
Findryankp <findryankurnia@gmail.com>
2023-03-12 21:28:05 +0700

    router rooms

---
+++ app/router/roomRouter.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+
+	"groupproject3-airbnb-api/app/config"
+	_roomData "groupproject3-airbnb-api/features/rooms/data"
+	_roomHandler "groupproject3-airbnb-api/features/rooms/handler"
+	_roomService "groupproject3-airbnb-api/features/rooms/service"
+)
+
+func RoomRouter(db *gorm.DB, e *echo.Echo) {
+	data := _roomData.New(db)
+	service := _roomService.New(data)
+	handler := _roomHandler.New(service)
+
+	g := e.Group("/rooms")
+	g.Use(echojwt.JWT([]byte(config.JWTKey)))
+	g.GET("", handler.GetAll)
+	g.GET("/:id", handler.GetById)
+	g.POST("", handler.Create)
+	g.PUT("/:id", handler.Update)
+	g.DELETE("/:id", handler.Delete)
+
+	g.GET("/users/:id/rooms", handler.GetByUserId)
+}
