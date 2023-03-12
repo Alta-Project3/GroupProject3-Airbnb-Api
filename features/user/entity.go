aee8bff4af463fc44ee5e113a2b39a658a349ae8
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-12 17:58:16 +0700

    chore : user entity

--- features/user/entity.go
+++ features/user/entity.go
@@ -1,1 +1,28 @@
 package user
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	ID             uint
+	ProfilePicture string
+	Name           string
+	Email          string
+	Password       string
+	Phone          string
+	Address        string
+}
+
+type UserHandler interface {
+	Register() echo.HandlerFunc
+	Login() echo.HandlerFunc
+}
+
+type UserService interface {
+	Register(newUser Core) (Core, error)
+	Login(email, password string) (string, Core, error)
+}
+
+type UserData interface {
+	Register(newUser Core) (Core, error)
+	Login(email string) (Core, error)
+}
