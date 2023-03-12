5a791bcaac10a9b9c9fc09c236ae9e3ed4e459ad
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-12 18:15:42 +0700

    chore : register in router

--- app/router/router.go
+++ app/router/router.go
@@ -1,1 +1,19 @@
 package router
+
+import (
+	usrData "groupproject3-airbnb-api/features/user/data"
+	usrHdl "groupproject3-airbnb-api/features/user/handler"
+	usrSrv "groupproject3-airbnb-api/features/user/services"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func InitRouter(db *gorm.DB, e *echo.Echo) {
+	uData := usrData.New(db)
+	uService := usrSrv.New(uData)
+	uHandler := usrHdl.New(uService)
+
+	// AUTH
+	e.POST("/register", uHandler.Register())
+}
