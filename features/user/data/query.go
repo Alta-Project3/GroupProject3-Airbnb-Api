b0b15cc2a83fa6f244b675095ba575f8fec71bc2
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-12 19:39:31 +0700

    feat : update in query

--- features/user/data/query.go
+++ features/user/data/query.go
@@ -64,9 +64,31 @@ func (uq *userQuery) Profile(userID uint) (user.Core, error) {
 	return ToCore(res), nil
 }
 
-// Update implements user.UserData
-func (*userQuery) Update(userID uint, updateData user.Core) (user.Core, error) {
-	panic("unimplemented")
+func (uq *userQuery) Update(userID uint, updateData user.Core) (user.Core, error) {
+	if updateData.Email != "" {
+		dupEmail := User{}
+		err := uq.db.Where("email = ?", updateData.Email).First(&dupEmail).Error
+		if err == nil {
+			log.Println("duplicated")
+			return user.Core{}, errors.New("email duplicated")
+		}
+	}
+
+	cnv := CoreToData(updateData)
+	qry := uq.db.Model(&User{}).Where("id = ?", userID).Updates(&cnv)
+	affrows := qry.RowsAffected
+	if affrows == 0 {
+		log.Println("no rows affected")
+		return user.Core{}, errors.New("no data updated")
+	}
+	err := qry.Error
+	if err != nil {
+		log.Println("update user query error", err.Error())
+		return user.Core{}, errors.New("user not found")
+	}
+	result := ToCore(cnv)
+	result.ID = userID
+	return result, nil
 }
 
 // Deactivate implements user.UserData
