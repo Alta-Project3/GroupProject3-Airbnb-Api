43818db0807b79437d39e20905754eb4a5128845
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-13 21:04:24 +0700

    fix : upgradehost in query

--- features/user/data/query.go
+++ features/user/data/query.go
@@ -114,17 +114,13 @@ func (uq *userQuery) Deactivate(userID uint) error {
 	return nil
 }
 
-func (uq *userQuery) UpgradeHost(userID uint, approvement string) (user.Core, error) {
-	getID := User{}
-	if approvement == "yes" {
-		getID.Role = "Host"
-	}
-
-	err := uq.db.Where("id = ?", userID).First(&getID).Error
+func (uq *userQuery) UpgradeHost(userID uint, approvement user.Core) error {
+	input := CoreToData(approvement)
+	err := uq.db.Where("id = ?", userID).Updates(&input).Error
 	if err != nil {
 		log.Println("get user error : ", err.Error())
-		return user.Core{}, errors.New("failed to upgrade to host")
+		return errors.New("failed to upgrade to host")
 	}
 
-	return ToCore(getID), nil
+	return nil
 }
