83a324c6d990ce7bd2bb3d8de364fb2475342150
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-14 11:14:36 +0700

    test : user login

--- features/user/services/service_test.go
+++ features/user/services/service_test.go
@@ -3,6 +3,7 @@ package services
 import (
 	"errors"
 	"groupproject3-airbnb-api/features/user"
+	"groupproject3-airbnb-api/helper"
 	"groupproject3-airbnb-api/mocks"
 	"testing"
 
@@ -46,6 +47,59 @@ func TestRegister(t *testing.T) {
 		err := srv.Register(inputData)
 		assert.NotNil(t, err)
 		assert.ErrorContains(t, err, "server")
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestLogin(t *testing.T) {
+	repo := mocks.NewUserData(t)
+	inputEmail := "alif@gmail.com"
+	passwordHashed := helper.GeneratePassword("123")
+	resData := user.Core{ID: uint(1), Name: "Alif", Email: "alif@gmail.com", Password: passwordHashed}
+
+	t.Run("login success", func(t *testing.T) {
+		repo.On("Login", inputEmail).Return(resData, nil).Once()
+		srv := New(repo)
+		token, res, err := srv.Login(inputEmail, "123")
+		assert.Nil(t, err)
+		assert.NotEmpty(t, token)
+		assert.Equal(t, resData.Name, res.Name)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("account not found", func(t *testing.T) {
+		repo.On("Login", inputEmail).Return(user.Core{}, errors.New("data not found")).Once()
+		srv := New(repo)
+		token, res, err := srv.Login(inputEmail, "123")
+		assert.NotNil(t, token)
+		assert.ErrorContains(t, err, "not")
+		assert.Empty(t, token)
+		assert.Equal(t, uint(0), res.ID)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("password not matched", func(t *testing.T) {
+		inputEmail := "lip23"
+		repo.On("Login", inputEmail).Return(resData, nil).Once()
+		srv := New(repo)
+		_, res, err := srv.Login(inputEmail, "342")
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "password")
+		assert.Empty(t, nil)
+		assert.Equal(t, uint(0), res.ID)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("server problem", func(t *testing.T) {
+		inputEmail := "alif23"
+		repo.On("Login", inputEmail).Return(user.Core{}, errors.New("There is a problem with the server")).Once()
+
+		srv := New(repo)
+		token, res, err := srv.Login(inputEmail, "342")
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "server")
+		assert.Empty(t, token)
+		assert.Equal(t, user.Core{}, res)
 		repo.AssertExpectations(t)
 	})
 }
