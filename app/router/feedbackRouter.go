9f73c6a15ac41a5471aa4fdf3b88f040d18378d7
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-15 15:10:41 +0700

    chore : get approval by id in router

--- app/router/feedbackRouter.go
+++ app/router/feedbackRouter.go
@@ -19,4 +19,5 @@ func feedbackRouter(db *gorm.DB, e *echo.Echo) {
 	//Feedback
 	e.POST("/feedbacks", fHandler.Create(), middleware.JWT([]byte(config.JWTKey)))
 	e.GET("/feedbacks", fHandler.GetUserFeedback(), middleware.JWT([]byte(config.JWTKey)))
+	e.GET("/feedbacks", fHandler.GetByID(), middleware.JWT([]byte(config.JWTKey)))
 }
