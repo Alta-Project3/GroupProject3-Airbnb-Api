00ae438cc3ea0d2637b506c63dd53149121cd1d2
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-15 12:30:54 +0700

    feat : feedback response

---
+++ features/feedback/handler/response.go
@@ -0,0 +1,19 @@
+package handler
+
+import "groupproject3-airbnb-api/features/feedback"
+
+type FeedbackResponse struct {
+	ID       uint    `json:"id,omitempty"`
+	RoomID   uint    `json:"room_id,omitempty"`
+	Rating   float64 `json:"rating,omitempty"`
+	Feedback string  `json:"feedback,omitempty"`
+}
+
+func ToResponse(data feedback.Core) FeedbackResponse {
+	return FeedbackResponse{
+		ID:       data.ID,
+		RoomID:   data.RoomID,
+		Rating:   data.Rating,
+		Feedback: data.Feedback,
+	}
+}
