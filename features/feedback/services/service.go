3b54b700853100e61b480a63559bda62fd830368
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-15 15:22:36 +0700

    feat : update feedback in service

--- features/feedback/services/service.go
+++ features/feedback/services/service.go
@@ -64,7 +64,21 @@ func (fuc *feedbackUseCase) GetByID(token interface{}, feedbackID uint) (feedbac
 	return res, nil
 }
 
-// Update implements feedback.FeedbackService
-func (*feedbackUseCase) Update(token interface{}, feedbackID uint) (feedback.Core, error) {
-	panic("unimplemented")
+func (fuc *feedbackUseCase) Update(token interface{}, feedbackID uint, updatedFeedback feedback.Core) (feedback.Core, error) {
+	userID := helper.ExtractToken(token)
+
+	if userID <= 0 {
+		return feedback.Core{}, errors.New("user not found")
+	}
+	res, err := fuc.qry.Update(uint(userID), feedbackID, updatedFeedback)
+
+	if err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			return feedback.Core{}, errors.New("data not found")
+		} else {
+			return feedback.Core{}, errors.New("internal server error")
+		}
+	}
+
+	return res, nil
 }
