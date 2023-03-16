87b1e53a602339da344ddc68d6f96a7150bc87a7
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-16 17:00:10 +0700

    test : get detail feedback

--- features/feedback/services/service_test.go
+++ features/feedback/services/service_test.go
@@ -87,3 +87,49 @@ func TestGetUserFeedback(t *testing.T) {
 	})
 
 }
+
+func TestGetByID(t *testing.T) {
+	repo := mocks.NewFeedbackData(t)
+
+	resData := feedback.Core{ID: 1, RoomID: 1, Rating: 3, Feedback: "nyaman"}
+
+	t.Run("success get feedback details", func(t *testing.T) {
+		repo.On("GetByID", uint(1), uint(1)).Return(resData, nil).Once()
+
+		srv := New(repo)
+		_, token := helper.GenerateJWT(1)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.GetByID(pToken, uint(1))
+		assert.Nil(t, err)
+		assert.Equal(t, resData.ID, res.ID)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("data not found", func(t *testing.T) {
+		repo.On("GetByID", uint(1), uint(1)).Return(feedback.Core{}, errors.New("data not found")).Once()
+		srv := New(repo)
+		_, token := helper.GenerateJWT(1)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.GetByID(pToken, uint(1))
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "not found")
+		assert.Equal(t, feedback.Core{}, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("server problem", func(t *testing.T) {
+		repo.On("GetByID", uint(1), uint(1)).Return(feedback.Core{}, errors.New("server problem")).Once()
+		srv := New(repo)
+		_, token := helper.GenerateJWT(1)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.GetByID(pToken, uint(1))
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "server")
+		assert.Equal(t, feedback.Core{}, res)
+		repo.AssertExpectations(t)
+	})
+
+}
